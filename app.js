4d3aade45cd490c3e18ac8bc2a4f8c0044c967ce
Volodymyr Basok <volodymyrquo@gmail.com>
2024-03-11 15:12:58 +0200

    add tests

--- app.js
+++ app.js
@@ -1,39 +1,58 @@
 const express = require('express');
-const {PrismaClient} = require('@prisma/client');
+const { PrismaClient } = require('@prisma/client');
 const Joi = require('joi');
 const prisma = new PrismaClient();
 
 const app = express();
 const port = 3000;
-
 app.use(express.json());
 app.use((req, res, next) => {
-    console.log(req.method, req.path);
+    console.log(req.method, req.url);
     next();
 });
 
-app.get('/users', async(req, res) => {
+app.get('/users', async (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 3;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
     try {
         const users = await prisma.user.findMany();
-        res.json(users);
-    
+        const usersSlice = users.slice(startIndex, endIndex);
+        res.json(usersSlice);
     } catch (err) {
         res.status(400).json({ error: err.message });
-
     }
 });
 
-app.get('/users/:id', async(req, res) => {
+app.get('/api/books', async (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    try {
+        const books = await prisma.user.findMany();
+        const booksSlice = books.slice(startIndex, endIndex);
+        res.json(booksSlice);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+});
+
+app.get('/users/:id', async (req, res) => {
     const { id } = req.params;
-    try{
+    try {
         const user = await prisma.user.findUnique({
             where: {
-                id: Number(id)
-            }
+                id: Number(id),
+            },
         });
-       if(user) {res.status(200).json(user);}
-       else {res.status(404).json({ message: 'Користувача не знайдено' });}
-    } catch(err){
+        if (user) {
+            res.status(200).json(user);
+        } else {
+            res.status(404).json({ message: 'Користувача не знайдено' });
+        }
+    } catch (err) {
         res.status(400).json({ error: err.message });
     }
 });
@@ -43,62 +62,65 @@ const userSchema = Joi.object({
 });
 app.post('/users', async (req, res) => {
     const userData = req.body;
-    const {value, error } = userSchema.validate(userData);
+    const { value, error } = userSchema.validate(userData);
     if (error) {
         return res.status(400).json({ error: error.message });
     }
     const { name, email } = value;
-    try{
+    try {
         const user = await prisma.user.create({
             data: {
                 name,
-                email
-            }
+                email,
+            },
         });
         res.status(201).json(user);
-    } catch(err){
-        res.status(400).json({ error: err.message });
-    }
-});
-
-app.delete('/users/:id', async(req, res) => {
-    const { id } = req.params;
-    try {
-        const user = await prisma.user.delete({
-            where: {
-                id: parseInt(id)
-            }
-        });
-        res.json({message: 'User deleted'});
     } catch (err) {
         res.status(400).json({ error: err.message });
     }
 });
 
-app.put('/users/:id', async(req, res) => {
+app.delete('/users/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const user = await prisma.user.delete({
+            where: {
+                id: parseInt(id),
+            },
+        });
+        res.json({ message: 'User deleted' });
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+});
+
+app.put('/users/:id', async (req, res) => {
     const { id } = req.params;
     const { name, email } = req.body;
-try {
-    const user = await prisma.user.update({
-        where: {
-            id: Number(id)
-        },
-        data: {
-            name,
-            email
-        }
-    });
-    res.json(user);
-} catch (err) {
-    res.status(400).json({ error: err.message });
-}
-    
+    try {
+        const user = await prisma.user.update({
+            where: {
+                id: Number(id),
+            },
+            data: {
+                name,
+                email,
+            },
+        });
+        res.json(user);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
 });
 
 app.get('/', (req, res) => {
     res.send('Welcome to my Express App!');
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
+if (require.main === module) {
+    app.listen(port, () => {
+        console.log(`Example app listening at http://localhost:${port}`);
+    });
+}
+
+module.exports = app;
